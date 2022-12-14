a44a3431385d8dd7cb0122ba3238603de4e7c854
zakoEssy <zakaressaiydy@gmail.com>
2022-12-14 10:45:52 +0100

    hada mahwa hada

--- src/main/java/com/kindsonthegenius/thymeleafapp/ThymeleafAppApplication.java
+++ src/main/java/com/kindsonthegenius/thymeleafapp/ThymeleafAppApplication.java
@@ -9,6 +9,8 @@ public class ThymeleafAppApplication {
 	//test the jenkins job if trrigre
 	//test building docker image whit jenkins in local repo
 	//hey from demi final
+	//hada mahwa hada
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(ThymeleafAppApplication.class, args);
